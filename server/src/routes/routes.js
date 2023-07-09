cb5efd29375ed3df62abeae5ace2da8738bcbd01
Jeff <david.arteaga@utp.edu.co>
2023-07-09 18:01:44 -0500

    Aqui incluyo la funcion para actualizar un product

--- server/src/routes/routes.js
+++ server/src/routes/routes.js
@@ -2,7 +2,7 @@ import bodyParser from "body-parser"
 import { Router } from "express"
 import cors from 'cors'
 
-import {login, sigin} from "../controller/formControllers.js"
+import {login, sigin, update} from "../controller/formControllers.js"
 
 const router = Router()
 
@@ -18,5 +18,7 @@ router.get("/", (req, res) => {
 router.post('/register',sigin)
 
 router.post('/login', login);
+
+
 
 export default router
