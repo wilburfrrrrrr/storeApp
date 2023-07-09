ece0c0d1b6153205e1f248c415013f26fd303010
SMejia115 <s.mejia6@utp.edu.co>
2023-07-09 13:48:47 -0500

    Initial Product View

--- client/src/App.js
+++ client/src/App.js
@@ -3,6 +3,8 @@ import './App.css';
 // import ProductElement from './Components/productElement';
 import Register from './Components/register';
 import Login from './Components/login';
+import ProductView from './Components/productView';
+
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'; 
 
 function App() {
@@ -13,7 +15,9 @@ function App() {
         <Route path='/login' element={<Login/>} />
         <Route path='/viewAdmin'/>
         <Route path='/viewUser'/>
+        {/* <Route path='/viewProduct' element={<ProductView/>} /> */}
       </Routes>
+      <ProductView />
     </Router>
   );
 }
--- client/src/Components/productView.jsx
+++ client/src/Components/productView.jsx
@@ -4,23 +4,32 @@ import '../Styles/productView.css';
 function ProductView(props) {
     return(
         <div className='mainContainer'>
-            <div className='infoProduct'>
+            <div className='productContainer'>
                 <div className='productImage'>
-                    <img src={props.product.image} alt='productImage' />
+                    <img src={require('../Images/productsImages/product1/2.jpg')} alt='productImage' />
                 </div>
 
-                <div className='priceContainer'>
-                    <h2>Precio</h2>
-                </div>
+                <div className='productInfo'>
+                    <div className='productName'>
+                        <h1>Nombre del Producto</h1>
+                    </div>
+                    <div className='productPrice'>
+                        <h2>Precio</h2>
+                    </div>
+                    <div className='productAmount'>
+                        <h3>CantDisponible</h3>
+                    </div>
+                    <div className='productButton'>
+                        <div></div>
+                        <input type='number'/>
+                        <button>Agregar al Carrito</button>
+                    </div>
 
-                <div>
-                    
                 </div>
-
             </div>
 
-            <div className='descriptionProduct'>
-
+            <div className='productDescription'>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Optio quasi corrupti itaque possimus iste eum enim dolor rerum quis voluptas, ea doloremque, vitae laboriosam voluptates sequi. Placeat totam unde neque. Lorem ipsum dolor sit amet consectetur adipisicing elit. Perferendis omnis eos corporis eaque fuga a sed maxime labore mollitia magni dolorum asperiores ea quod aut voluptate, quo officia iusto laborum. Lorem ipsum dolor sit amet consectetur adipisicing elit. Ad nostrum repellendus nulla tempore rerum, ipsum sequi soluta optio aliquid aperiam, nihil aut consequatur non dolore reprehenderit delectus aspernatur ut accusantium! Lorem ipsum dolor sit amet, consectetur adipisicing elit. Mollitia enim odit, placeat commodi ad inventore ipsam facere dolorum ipsum voluptatibus culpa quo aperiam tenetur sapiente quis nobis blanditiis. Cumque, magnam?</p>
             </div>
 
         </div>
