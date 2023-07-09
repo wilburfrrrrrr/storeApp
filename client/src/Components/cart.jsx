497a182ca9f581475151540b273391f553f7874a
andrepalacio <andres.palacio@utp.edu.co>
2023-07-09 16:00:47 -0500

    merge 2

---
+++ client/src/Components/cart.jsx
@@ -0,0 +1,39 @@
+import React from 'react';
+import '../Styles/cart.css';
+import productImg from '../Images/productsImages/product1/1.jpg';
+
+function CartItem() {
+    return (
+        <div className='item'>
+            <div className='itemImage'>
+                <img src={productImg} alt='product' />
+            </div>
+            <div className='itemInfo'>
+                <h2>Product</h2>
+                <p className='itemQty'>Qty: 1</p>
+                <p className='itemPrice'>9.99</p>
+                <button className='itemRemove'>Quitar</button>
+            </div>
+        </div>
+)}
+
+function Cart() {
+  return (
+    <>
+        <h1 className='cartTitle'>Shopping Cart</h1>
+        <div className='cart'>
+            <div className='cartItems'>
+                <CartItem />
+                {/* <CartItem /> */}
+            </div>
+            <div className='cartTotal'>
+                <p className='cartTotalTitle'>Total:</p>
+                <p className='cartTotalPrice'>$9.99</p>
+                <button>Pagar</button>
+            </div>
+        </div>
+    </>
+  )
+}
+
+export default Cart
--- client/src/Components/login.jsx
+++ client/src/Components/login.jsx
@@ -1,7 +1,7 @@
 import '../Styles/login.css';
-import React, {useState} from 'react'
-import logo from '../Images/appImages/Logo.jpeg';
-import axios from 'axios'
+import React, {useState} from 'react';
+import logo from '../Images/appImages/logo.jpeg';
+import axios from 'axios';
 
 const CompShowLogin = () => { // Se define el componente.
 
