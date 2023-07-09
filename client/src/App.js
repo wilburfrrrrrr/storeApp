af5d4492fe70fb6c0db10712f031c57a67e67459
Jeff <david.arteaga@utp.edu.co>
2023-07-09 10:26:05 -0500

    Me vuelvo a poner al dia

--- client/src/App.js
+++ client/src/App.js
@@ -1,16 +1,8 @@
 import React from 'react';
-<<<<<<< HEAD
-import './Styles/App.css';
-//import CompShowLogin from './Components/login';
-import ProductElement from './Components/productElement';
-import Register from './Components/register';
-import Login from './Components/login';
-=======
 import './App.css';
 // import ProductElement from './Components/productElement';
-import Register from './Components/Register';
-import Login from './Components/Login';
->>>>>>> origin/main
+import Register from './Components/register';
+import Login from './Components/login';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'; 
 
 function App() {
