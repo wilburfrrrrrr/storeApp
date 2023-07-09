2d8e0595f31288fffc3772e260d96dd149e8935a
Jeff <david.arteaga@utp.edu.co>
2023-07-09 06:35:51 -0500

    Aqui ya manda peticiones post

--- server/src/index.js
+++ server/src/index.js
@@ -1,9 +1,14 @@
 import express from "express";
 import mysql from "mysql";
 import path from "path";
+import bodyParser from "body-parser";
+import cors from 'cors'
 
 const app = express();
 const PORT = 9000;
+
+app.use(bodyParser.json())
+app.use(cors())
 
 const db = mysql.createConnection({
   host: "localhost",
@@ -16,6 +21,14 @@ app.get("/", (req, res) => {
   res.sendFile(path.resolve("../index.html"));
 });
 
+app.post('/register', (req, res) => {
+  const requesData = req.body
+  console.log(requesData);
+  res.json({message: 'Peticion POST recibida para Andres'})
+})
+
+
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
+
