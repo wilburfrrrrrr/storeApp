8967c2e963d94fb405748302925825067f77a6b6
andrepalacio <andres.palacio@utp.edu.co>
2023-07-09 18:32:57 -0500

    server merge

--- server/src/routes/routes.js
+++ server/src/routes/routes.js
@@ -1,2 +1,29 @@
+import bodyParser from "body-parser"
+import { Router } from "express"
+import cors from 'cors'
+
+import {login, sigin, update} from "../controller/formControllers.js"
+import {administrator} from "../controller/administrator.js"
+
+const router = Router()
+
+router.use(bodyParser.urlencoded({extended: false}))
+router.use(bodyParser.json())
+router.use(cors())
+
+router.get("/", (req, res) => {
+    res.sendFile(path.resolve("../index.html"));
+  });
+
+
+router.post('/register',sigin)
+
+router.post('/login', login);
+
+router.put('/update', update);
+
+router.get('/administrator', administrator);
+
+
 
 export default router
