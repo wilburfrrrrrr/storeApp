ad14d6b5cab0243dc60f2ee55ade85c75df764ae
andrepalacio <andres.palacio@utp.edu.co>
2023-07-08 21:10:55 -0500

    first commit

---
+++ storeDB.sql
@@ -0,0 +1,32 @@
+CREATE DATABASE theStore;
+USE theStore;
+
+create table users (
+	id int auto_increment primary key,
+    name varchar(70) not null,
+    email varchar(70) not null,
+    password varchar(70) not null
+);
+
+select * from users;
+
+create table administrators (
+	id int auto_increment primary key,
+    name varchar(70) not null,
+    password varchar(70) not null,
+    email varchar(70) not null,
+    address varchar(70),
+    phone int
+);
+
+insert into administrators (name, password, email, address, phone)
+values ("generalAdmin", "encode%987", "none@mail.com", "at the office", 300);
+
+select * from administrators;
+
+/*NO EJECUTAR ESTA PARTE*/
+/*Administradores opcionales, se considerará inserción en la tabla posteriormente*/
+insert into administrators (name, password, email, address, phone)
+values ("andres", "decode@810", "andres.palacio@utp.edu.co", "pereira", 3013004022);
+insert into administrators (name, password, email, address, phone)
+values ("jefferson", "hacking#77", "david.arteaga@utp.edu.co", "dosquebradas", 3185002087);
