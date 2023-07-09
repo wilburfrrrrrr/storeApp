dd5093029c25dfd61b0a52e91637694985949624
andrepalacio <andres.palacio@utp.edu.co>
2023-07-09 15:03:42 -0500

    prediseño carro

--- client/src/App.js
+++ client/src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 // import ProductElement from './Components/productElement';
 import Register from './Components/register';
 import Login from './Components/login';
+import Cart from './Components/cart';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'; 
 
 function App() {
@@ -11,6 +12,7 @@ function App() {
       <Routes>
         <Route path='/register' element={<Register/>} />
         <Route path='/login' element={<Login/>} />
+        <Route path='/' element={<Cart />} />
       </Routes>
     </Router>
   );
