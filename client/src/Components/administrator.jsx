1bc5c5a55824c9c4321aa9ce7d55f3dca2361bf0
andrepalacio <andres.palacio@utp.edu.co>
2023-07-09 21:09:28 -0500

    avance admin

--- client/src/Components/administrator.jsx
+++ client/src/Components/administrator.jsx
@@ -1,4 +1,6 @@
 import React, {useEffect, useState} from 'react';
+import Stock from './stock';
+import '../Styles/administrator.css';
 
 function Administrator() {
   const [admin, setAdmin] = useState(null);
@@ -19,11 +21,35 @@ function Administrator() {
 
   return (
     <>
-      <div>Administrator</div>
+      <div className='adminData'>
+        <h2>Bienvenido {admin.admin[0].name}</h2>
+        <form method='post'>
+          <div>
+            <label htmlFor='name'>Nombre</label>
+            <input type='text' name='name' id='name' disabled value={admin.admin[0].name} />
+          </div>
+          <div>
+            <label htmlFor='email'>Email</label>
+            <input type='email' name='email' id='email' value={admin.admin[0].email} />
+          </div>
+          <div>
+            <label htmlFor='password'>Contraseña</label>
+            <input type='password' name='password' id='password' value={admin.admin[0].password} />
+          </div>
+          <div>
+            <label htmlFor='phone'>Teléfono</label>
+            <input type='tel' name='phone' id='phone' value={admin.admin[0].phone} />
+          </div>
+          <div>
+            <label htmlFor='address'>Dirección</label>
+            <input type='text' name='address' id='address' value={admin.admin[0].address} />
+          </div>
+          <div><button type='submit'>Editar</button></div>
+        </form>
+      </div>
       {admin && (
         <>
-          <p>Admin ID: {admin.admin[0].name}</p>
-          <p>Number of products: {admin.products.length}</p>
+          <Stock productsArray={admin.products} />
         </>
       )}
     </>
