914d1d7b551f07577e0e7f4c5e47bb7dc853ece5
andrepalacio <andres.palacio@utp.edu.co>
2023-07-13 01:17:13 -0500

    admin control

--- client/src/Components/administratorForm.jsx
+++ client/src/Components/administratorForm.jsx
@@ -1,27 +1,52 @@
+//create a function to change administrator data, on change
+
+import React, {useState} from 'react';
+
 function AdministratorForm(admin) {
+
+  const [formData, setFormData] = useState(admin.admin[0]);
+  const [showPassword, setShowPassword] = useState(false);
+
+  const handleInputChange = (e) => {
+    if (e && e.target) {
+      const { name, value } = e.target;
+      setFormData((prevData) => ({
+        ...prevData,
+        [name]: value,
+      }));
+    }
+  };
+
+  const toggleShowPassword = () => {
+    setShowPassword((prevState) => !prevState);
+  };
+
   return (
     <div className='adminData'>
-      <h2>Bienvenido {admin.admin[0].name}</h2>
       <form method='post'>
         <div>
           <label htmlFor='name'>Nombre</label>
-          <input type='text' name='name' id='name' disabled value={admin.admin[0].name} />
+          <input type='text' name='name' id='name' disabled value={formData.name} />
         </div>
         <div>
           <label htmlFor='email'>Email</label>
-          <input type='email' name='email' id='email' value={admin.admin[0].email} />
+          <input type='email' name='email' id='email' value={formData.email} onChange={handleInputChange}/>
         </div>
         <div>
           <label htmlFor='password'>Contraseña</label>
-          <input type='password' name='password' id='password' value={admin.admin[0].password} />
+          <input type='password' name='password' id='password' value={formData.password} onChange={handleInputChange} />
+        </div>
+        <div>
+          <input type='checkbox' id='showPassword' checked={showPassword} onChange={toggleShowPassword} />
+          <label htmlFor='showPassword'>Mostrar contraseña</label>
         </div>
         <div>
           <label htmlFor='phone'>Teléfono</label>
-          <input type='tel' name='phone' id='phone' value={admin.admin[0].phone} />
+          <input type='tel' name='phone' id='phone' value={formData.phone} onChange={handleInputChange}/>
         </div>
         <div>
           <label htmlFor='address'>Dirección</label>
-          <input type='text' name='address' id='address' value={admin.admin[0].address} />
+          <input type='text' name='address' id='address' value={formData.address} onChange={handleInputChange}/>
         </div>
         <button type='submit'>Editar</button>
       </form>
--- client/src/Components/stock.jsx
+++ client/src/Components/stock.jsx
@@ -1,7 +1,19 @@
-import '../Styles/stock.css'
+import React, { useState } from 'react';
+import '../Styles/stock.css';
 
 function Stock(productsArray) {
-  let products = productsArray.productsArray;
+  const [products, setProducts] = useState(productsArray.productsArray);
+
+  const handleInputChange = (e, productId, field) => {
+    const updatedProducts = products.map((product) => {
+      if (product.id === productId) {
+        return { ...product, [field]: e.target.value };
+      }
+      return product;
+    });
+    setProducts(updatedProducts);
+  };
+
   return (
     <table>
       <thead>
@@ -15,18 +27,52 @@ function Stock(productsArray) {
         </tr>
       </thead>
       <tbody>
-      {products.map((product) => (
-        <tr key={product.id}>
-          <td><input type="text" disabled value={product.id}/></td>
-          <td><input type="text" disabled value={product.name}/></td>
-          <td><input type="number" min={0} value={product.price} /></td>
-          <td><input type="number" min={0} value={product.amount} /></td>
-          <td><input type="number" min={0} value={product.minStock} /></td>
-          <td><button>Editar</button></td>
-        </tr>
-      ))}
-    </tbody>
-  </table>
-)}
+        {products.map((product) => (
+          <tr key={product.id}>
+            <td>
+              <input type="text" disabled value={product.id} />
+            </td>
+            <td>
+              <input type="text" disabled value={product.name} />
+            </td>
+            <td>
+              <input
+                type="number"
+                min={0}
+                value={product.price}
+                onChange={(e) =>
+                  handleInputChange(e, product.id, 'price')
+                }
+              />
+            </td>
+            <td>
+              <input
+                type="number"
+                min={0}
+                value={product.amount}
+                onChange={(e) =>
+                  handleInputChange(e, product.id, 'amount')
+                }
+              />
+            </td>
+            <td>
+              <input
+                type="number"
+                min={0}
+                value={product.minStock}
+                onChange={(e) =>
+                  handleInputChange(e, product.id, 'minStock')
+                }
+              />
+            </td>
+            <td>
+              <button>Editar</button>
+            </td>
+          </tr>
+        ))}
+      </tbody>
+    </table>
+  );
+}
 
 export default Stock;
