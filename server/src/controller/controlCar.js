f42dfdaeb610af66641120f6de318a963604e76c
ossa <bleedingbearclaws@gmail.com>
2023-07-13 21:49:41 -0500

    Modificacion de carro implementado

--- server/src/controller/controlCar.js
+++ server/src/controller/controlCar.js
@@ -20,11 +20,6 @@ CREATE TABLE IF NOT EXISTS puchaseList(
   productId
   amount
 );
-
-
-id = req.id
-queryCarOpen 
-queryVerifyUser 'SELECT'
 */
 
 const stripeSecret = "sk_test_51NS4P4KVzQlPajzBoWrdb25nCwhexkdZe8E1qvNIDGOaEEEvqxzzomsGg8pcGwkazZRrMyhcvWLbhiMpPl5pgHhd00S8mgl93p"
@@ -50,34 +45,57 @@ export const addCar = async (req, res) => {
 	const queryReserve          = 'UPDATE products SET amount = ? WHERE id = ?';
   const queryVerifyCar        = 'SELECT * FROM userCar WHERE userId = ?';
   const queryInsertCar        = 'INSERT INTO userCar (userId, date) VALUES (?, ?)';
-  const queryVerifyProduct    = 'SELECT * FROM purchaseList WHERE productId = ?';
-  const queryIncreaseProduct  = 'UPDATE purchaseList SET amount = ? WHERE productId = ?';
-  const queryAddProduct       = 'INSERT INTO purchaseList (id, carId, productId, amount) VALUES (?, ?, ?, ?)';
-  const queryAddCar           = 'INSERT INTO ';
+  const queryVerifyProduct    = 'SELECT * FROM purchaseList WHERE carId = ? and productId = ?';
+  const queryIncreaseProduct  = 'UPDATE purchaseList SET amount = ? WHERE carId = ? and productId = ?';
+  const queryAddProduct       = 'INSERT INTO purchaseList (carId, productId, amount) VALUES (?, ?, ?)';
 
   db.query(queryVerifyCar, userId)
-  .then(result => {
-    if (result.length == 0){
+  .then(carAssigned => {
+    if (carAssigned.length == 0){
       db.queryInsertCar(queryInsertCar, [userId, curdate()])
-      .then(() => {
+      .then((carInserted) => {
         console.log("Carro asignado a usuario")
+        db.query(querySelect, )////////////////////////////////////////////////////////////////////??
+        .then((productSelected) => {
+          db.query(queryReserve, [productSelected[0].id, productSelected[0].amount - 1])
+          .then(console.log("Producto reservado"))
+          .catch(console.error("Error al realizar la reserva"))
+          db.query(queryVerifyProduct, [carInserted.insertId, productSelected[0].id])
+          .then((listResult) => {
+            if(listResult.result == 0){
+              db.query(queryAddProduct, [carInserted.insertId, productSelected[0].id, 1])
+              .catch(console.error("Error al modificar la lista de compra"))
+            }else{
+              db.query(queryIncreaseProduct, [listResult[0].amount + 1, carInserted.insertId, productSelected[0].id])
+            }
+          })
+          .catch(console.error("Error al buscar el producto en la lista de compras"))
+        })
+        .catch(console.error("Error al acceder al producto"))
       })
       .catch(console.error("Error al asignar carro a usuario"))
+    }else{
+      db.query(querySelect, )////////////////////////////////////////////////////////////////////??
+        .then((productSelected) => {
+          db.query(queryReserve, [productSelected[0].id, productSelected[0].amount - 1])
+          .then(console.log("Producto reservado"))
+          .catch(console.error("Error al realizar la reserva"))
+          db.query(queryVerifyProduct, [carAssigned.insertId, productSelected[0].id])
+          .then((listResult) => {
+            if(listResult.result == 0){
+              db.query(queryAddProduct, [carAssigned.insertId, productSelected[0].id, 1])
+              .catch(console.error("Error al modificar la lista de compra"))
+            }else{
+              db.query(queryIncreaseProduct, [listResult[0].amount + 1, carAssigned.insertId, productSelected[0].id])
+            }
+          })
+          .catch(console.error("Error al buscar el producto en el carrito de compras"))
+        })
+        .catch(console.error("Error al acceder al producto"))
     }
   },
-    db.query(querySelect, )
-    .then(results => {
-
-    })
-    .catch(console.error("Error al buscar el producto en el inventario"))
   )
-  .catch(console.error("Error al acceder al registro de carros"))
-  
-
-  
-  
-  
-
+  .catch(console.error("Error al acceder al registro de carros de compra"))
 }
 
 
@@ -85,34 +103,78 @@ export const removeCar = (req, res) => {
 	const token   = localStorage.getItem('accessToken');
   const userId  = token.id;
 	const querySelect           = 'SELECT * FROM products WHERE name = ?';
-	const queryReserve          = 'UPDATE products SET minStock = ? WHERE id = ?';
+	const queryReserve          = 'UPDATE products SET amount = ? WHERE id = ?';
   const queryVerifyCar        = 'SELECT * FROM userCar WHERE userId = ?';
-  const queryInsertCar        = 'INSERT INTO userCar (id, userId, date) VALUES (?, ?, ?)';
-  const queryVerifyProduct    = 'SELECT * FROM purchaseList WHERE productId = ?';
-  const queryIncreaseProduct  = 'UPDATE purchaseList SET amount = ? WHERE productId = ?';
-  const queryAddCar           = 'INSERT INTO purchaseList (id, carId, productId, amount) VALUES (?, ?, ?, ?)';
+  const queryVerifyProduct    = 'SELECT * FROM purchaseList WHERE carId = ? and productId = ?';
+  const queryDecreaseProduct  = 'UPDATE purchaseList SET amount = ? WHERE carId = ? and productId = ?';
+  const queryRemoveCar        = 'DELETE FROM purchaseList WHERE carId = ? and productId = ?';
+
+  db.query(queryVerifyCar, userId)
+  .then(carAssigned => {
+    db.query(querySelect, ) ////////////////////////////////////////////////////////////////////??
+    .then(productSelected => {
+      db.query(queryReserve, [productSelected[0].amount + 1, productSelected[0].id])
+      .then(console.log("Reserva anulada"))
+      .catch(console.error("Error al acceder a la reserva"))
+      db.query(queryVerifyProduct, [carAssigned.insertId, productSelected[0].id])
+      .then(listResult => {
+        if (listResult[0].amount > 1){
+          db.query(queryDecreaseProduct, [listResult[0].amount + 1, carAssigned.insertId, productSelected[0].id])
+          .catch(console.error("Error al modificar el carrito de compras"))
+        }else{
+          db.query(queryRemoveCar, [carAssigned.insertId, productSelected[0].id])
+        }
+      })
+      .catch(console.error("Error al acceder a la lista de productos"))
+    })
+    .catch(console.error("Error al acceder al producto"))
+  })
+  .catch(console.error("Error al acceder al registro de carros de compra"))
 }
   
 
-const checkInventory = (stock, req, res) => {
-  const queryAmount = 'SELECT amount FROM products WHERE id = ?';
+const checkInventory = (productId) => {
+  const queryAmount   = 'SELECT amount FROM products WHERE id = ?';
   const queryMinStack = 'SELECT minStock FROM products WHERE id = ?';
   // const minStock = db.query('SELECT minStock FROM products WHERE id = ?', )
-  const minStock = 5, maxStock = 30;
-  // const stock = product.minStock;
-  if (stock >= maxStock || stock <= minStock){
-    alertMail();
-  }
-  
+  db.query(queryMinStack, productId)
+  .then((productAmount) => {
+    db.query(queryAmount, productId)
+    .then((result) => {
+      if (result <= productAmount){
+        alertMail();
+      }
+    })
+    .catch(console.error("Error al acceder al stock del producto"))
+  })
+  .catch(console.error("Error al acceder al stock del producto"));
 }
 
 export const calculatePrice = (req, res) => {
   let prices = 0;
-  
-  for(let countPos = 0; countPos < purchaseList.length; countPos++){
-    prices += purchaseList[countPos].price;
-    checkInventory(purchaseList[countPos].minStock);
-  }
+  const token   = localStorage.getItem('accessToken');
+  const userId  = token.id;
+  const queryCount      = 'SELECT COUNT(*) FROM purchaseList WHERE userId = ?';
+  const queryPrice      = 'SELECT price FROM products WHERE id = ?';
+  const queryVerifyCar  = 'SELECT * FROM userCar WHERE userId = ?';
+
+
+  db.query(queryCount, userId)
+  .then((rows) => {
+    for(let idCount = 0; idCount < rows; idCount++){
+      db.query(queryPrice, idCount)
+      .then(result => {
+        price += result;
+        checkInventory(idCount);
+      })
+      .catch(console.error("Error al calcular el precio total"))
+    }
+  })
+  .catch(console.error("Error al acceder a la lista de compras"))
+  // for(let countPos = 0; countPos < purchaseList.length; countPos++){
+  //   prices += purchaseList[countPos].price;
+  //   checkInventory(purchaseList[countPos].minStock);
+  // }
   return prices;
 }
 
@@ -163,7 +225,10 @@ export const createSession = async (req, res) => {
 
 export const cancelPurchaseList = (req, res) => {
 	let countPos = 0;
-	const query = 'UPDATE products SET minStock = ? WHERE id = ?';
+  const token   = localStorage.getItem('accessToken');
+  const userId  = token.id;
+  const queryTruncate      = 'DELETE FROM purchaseList';
+	const queryCancelReserve = 'UPDATE products SET minStock = ? WHERE id = ?';
   
 	while(countPos < purchaseList.length){
 	  db.query(query, [purchaseList[countPos].minStock + 1, purchaseList[countPos].id])
@@ -172,12 +237,13 @@ export const cancelPurchaseList = (req, res) => {
 	  purchaseList.pop();
 	  countPos++;
 	}
-  }
+}
   
-  export const cleanPurchaseList = (req, res) => {
-	let countPos = 0;
-	while(countPos < purchaseList.length){
-	  purchaseList.pop();
-	  countPos++;
-	}
-  }
+export const cleanPurchaseList = (req, res) => {
+  const token   = localStorage.getItem('accessToken');
+  const userId  = token.id;
+  const queryTruncate = 'DELETE FROM purchaseList'
+
+  db.query(queryTruncate)
+  .then
+}
